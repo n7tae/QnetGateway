bf3ca7fec7c4fb32d7e21449ea770ac0d4e7bc2a
Tom Early <ac2ie@nycap.rr.com>
2014-06-21 14:55:29 -0400

    Update get_reflectors.sh

--- get_reflectors.sh
+++ get_reflectors.sh
@@ -1,6 +1,6 @@
 #/bin/bash
-# Get the big list from Ramesh (VA3UV) and extract the DStar and XReflectors only.
-# Put everything on port 20001 so no ports have to be opened!
+# Get the big list from Ramesh (VA3UV) and extract the DCS, DStar and XReflectors only.
+# Put XREF reflectors on port 20001 so no ports have to be forwarded on your home router!
 #
 # 73
 #
@@ -16,6 +16,7 @@ wget -nv -O gwys.va3uv.txt http://www.va3uv.com/gwys.txt
 
 if [ -e gwys.va3uv.txt ]; then
 	awk '$1~/^REF|XRF/&&$2~/[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/{print $1, $2, 20001}' gwys.va3uv.txt > gwys.txt
+	awk '$1~/^DCS/{print $1, $2, $3}' gwys.va3uv.txt >> gwys.txt
 else
         echo "Could not get gateways list from www.va3uv.com!"
 	if [ -e gwys.txt.orig ]; then
