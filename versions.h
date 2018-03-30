dc6bea7afd3eec3136864036bb85eb5a7d6ffaf3
Tom Early <n7tae@tearly.net>
2018-03-30 06:39:56 -0700

    g2 version update

--- versions.h
+++ versions.h
@@ -1,5 +1,5 @@
 // version strings must be 55 characters or less!
-#define IRCDDB_VERSION "linux-g2_ircddb-6.0.0"
+#define IRCDDB_VERSION "linux-g2_ircddb-6.0.1"
 #define LINK_VERSION "5.1.2"
 #define DVAP_VERSION "linux-dvap_rptr-5.1.1"
 #define DVRPTR_VERSION "linux-dvrptr-5.1.0"
