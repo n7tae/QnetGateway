99d1609f6b9744023d44d898fc1ff82d6164a270
Tom Early <n7tae@tearly.net>
2018-04-05 11:38:44 -0700

    typo in reflist.sh

--- reflist.sh
+++ reflist.sh
@@ -10,4 +10,4 @@ awk '$1 ~ /^REF/ { printf "%s %s 20001\n", $1, $2 }' DPlus_Hosts.txt >> gwys.txt
 awk '$1 ~ /^XRF/ { printf "%s %s 30001\n", $1, $2 }' DExtra_Hosts.txt >> gwys.txt
 awk '$1 ~ /^DCS/ { printf "%s %s 30051\n", $1, $2 }' DCS_Hosts.txt >> gwys.txt
 
-/bin/rm -f D{Extra,Plus,DSC}_Hosts.txt
+/bin/rm -f D{Extra,Plus,DCS}_Hosts.txt
