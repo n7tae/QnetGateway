f285e759916d0b112a7db064bf70f4a66b344932
Tom Early <n7tae@tearly.net>
2018-04-05 11:29:22 -0700

    reflist.sh update

--- reflist.sh
+++ reflist.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-wget ftp://dschost1.w6kd.com/DExtra_Hosts.txt
-wget ftp://dschost1.w6kd.com/DPlus_Hosts.txt
-wget ftp://dschost1.w6kd.com/DCS_Hosts.txt
-
+wget http://www.pistar.uk/downloads/DPlus_Hosts.txt
+wget http://www.pistar.uk/downloads/DExtra_Hosts.txt
+wget http://www.pistar.uk/downloads/DCS_Hosts.txt
 /bin/rm -f gwys.txt
 
-echo "# Downloaded from dschost1.w6kd.com `date`" > gwys.txt
+echo "# Downloaded from www.pistar.uk `date`" > gwys.txt
 awk '$1 ~ /^REF/ { printf "%s %s 20001\n", $1, $2 }' DPlus_Hosts.txt >> gwys.txt
 awk '$1 ~ /^XRF/ { printf "%s %s 30001\n", $1, $2 }' DExtra_Hosts.txt >> gwys.txt
 awk '$1 ~ /^DCS/ { printf "%s %s 30051\n", $1, $2 }' DCS_Hosts.txt >> gwys.txt
