fe1199d88346c9fddfc530c62b2ce45987f8743d
Tom Early <n7tae@tearly.net>
2018-04-08 21:51:00 -0700

    removed mmdvm log

--- mmdvm_modem.cpp
+++ mmdvm_modem.cpp
@@ -307,7 +307,7 @@ bool CMMDVMModem::ProcessMMDVM(const int len, const unsigned char *raw)
 				printf("ERROR: ProcessMMDVM: Could not write gateway header packet\n");
 				return true;
 			}
-			printf("INFO: ProcessMMDVM: sent header to port %u pkt = '%s\n", G2_IN_PORT, std::string((char *)gpkt.vpkt.hdr.rpt1, 36).c_str());
+//			printf("INFO: ProcessMMDVM: sent header to port %u pkt = '%s\n'", G2_IN_PORT, std::string((char *)gpkt.vpkt.hdr.rpt1, 36).c_str());
 		} else if (21 == len) {	// ambe
 			gpkt.remaining = 0x16;
 			memcpy(gpkt.vpkt.vasd.voice, mpkt.voice.ambe, 12);
