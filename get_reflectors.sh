0074b048efca6dc93a6133d40de3b59de481d3f7
Tom Early <n7tae@tearly.net>
2018-04-16 14:55:37 -0700

    moved back to va3uv.com; MMDVM.README #13

--- get_reflectors.sh
+++ get_reflectors.sh
@@ -1,10 +1,11 @@
 #/bin/bash
+
 # Get the big list from Ramesh (VA3UV) and extract the DCS, DStar and XReflectors only.
-# Put XREF reflectors on port 20001 so no ports have to be forwarded on your home router!
+# Put XREF reflectors on port 20001 so they will use DPlus linking!
 #
 # 73
 #
-# Tom, n7tae@arrl.net
+# Tom, n7tae (at) arrl (dot) net
 
 if [ -e gwys.txt ]; then
 	mv -f gwys.txt gwys.txt.orig
@@ -15,14 +16,15 @@ rm -f gwys.va2uv.txt
 wget -nv -O gwys.va3uv.txt http://www.va3uv.com/gwys.txt
 
 if [ -e gwys.va3uv.txt ]; then
+	echo "# from www.va3uv.com on `date`" > gwys.txt
+	echo "Got `awk '$1~/^REF/{print $1, $2, $3}' gwys.va3uv.txt | tee -a gwys.txt | wc -l` REF reflectors"
 	# Move DPlus and DExtra to port 20001
-	awk '$1~/^REF|XRF/{print $1, $2, 20001}' gwys.va3uv.txt > gwys.txt
-	# Get the DCS reflectors too
-	awk '$1~/^DCS/{print $1, $2, $3}' gwys.va3uv.txt >> gwys.txt
+	echo "Got `awk '$1~/^XRF/{print $1, $2, 20001}' gwys.va3uv.txt | tee -a gwys.txt | wc -l` XRF reflectors"
+	echo "Got `awk '$1~/^DCS/{print $1, $2, $3}' gwys.va3uv.txt | tee -a gwys.txt | wc -l` DCS reflectors"
 else
-        echo "Could not get gateways list from www.va3uv.com!"
+	echo "Could not get gateways list from www.va3uv.com!"
 	if [ -e gwys.txt.orig ]; then
-        	mv -f gwys.txt.orig gwys.txt
+		mv -f gwys.txt.orig gwys.txt
 	fi
 fi
 
--- reflist.sh
+++ reflist.sh
@@ -1,13 +1,29 @@
-#!/bin/bash
-# from the PiStar servers...
-wget http://www.pistar.uk/downloads/DPlus_Hosts.txt
-wget http://www.pistar.uk/downloads/DExtra_Hosts.txt
-wget http://www.pistar.uk/downloads/DCS_Hosts.txt
-/bin/rm -f gwys.txt
+#/bin/bash
 
-echo "# Downloaded from www.pistar.uk `date`" > gwys.txt
-awk '$1 ~ /^REF/ { printf "%s %s 20001\n", $1, $2 }' DPlus_Hosts.txt >> gwys.txt
-awk '$1 ~ /^XRF/ { printf "%s %s 30001\n", $1, $2 }' DExtra_Hosts.txt >> gwys.txt
-awk '$1 ~ /^DCS/ { printf "%s %s 30051\n", $1, $2 }' DCS_Hosts.txt >> gwys.txt
+# Get the big list from Ramesh (VA3UV) and extract the DCS, DStar and XReflectors only.
+# Put XREF reflectors on port 20001 so they will use DPlus linking!
+#
+# 73
+#
+# Tom, n7tae (at) arrl (dot) net
 
-/bin/rm -f D{Extra,Plus,DCS}_Hosts.txt
+if [ -e gwys.txt ]; then
+	mv -f gwys.txt gwys.txt.orig
+fi
+
+rm -f gwys.va2uv.txt
+
+wget -nv -O gwys.va3uv.txt http://www.va3uv.com/gwys.txt
+
+if [ -e gwys.va3uv.txt ]; then
+	echo "# from www.va3uv.com on `date`" > gwys.txt
+	echo "Got `awk '$1~/^REF/{print $1, $2, $3}' gwys.va3uv.txt | tee -a gwys.txt | wc -l` REF reflectors"
+	echo "Got `awk '$1~/^XRF/{print $1, $2, $3}' gwys.va3uv.txt | tee -a gwys.txt | wc -l` XRF reflectors"
+	echo "Got `awk '$1~/^DCS/{print $1, $2, $3}' gwys.va3uv.txt | tee -a gwys.txt | wc -l` DCS reflectors"
+else
+	echo "Could not get gateways list from www.va3uv.com!"
+	if [ -e gwys.txt.orig ]; then
+		mv -f gwys.txt.orig gwys.txt
+	fi
+fi
+
