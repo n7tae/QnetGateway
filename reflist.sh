cb36c9eda22f1270ddcc882b9e9f7780d566fa45
Tom Early <n7tae@tearly.net>
2018-04-24 17:31:05 -0700

    new qndtmf.sh and reflist.sh

--- reflist.sh
+++ reflist.sh
@@ -1,29 +1,14 @@
-#/bin/bash
+#!/bin/bash
 
-# Get the big list from Ramesh (VA3UV) and extract the DCS, DStar and XReflectors only.
-# Put XREF reflectors on port 20001 so they will use DPlus linking!
-#
-# 73
-#
-# Tom, n7tae (at) arrl (dot) net
+wget ftp://dschost1.w6kd.com/DExtra_Hosts.txt || wget ftp://dschost2.w6kd.com/DExtra_Hosts.txt
+wget ftp://dschost1.w6kd.com/DPlus_Hosts.txt || wget ftp://dschost2.w6kd.com/DPlus_Hosts.txt
+wget ftp://dschost1.w6kd.com/DCS_Hosts.txt || wget ftp://dschost2.w6kd.com/DCS_Hosts.txt
 
-if [ -e gwys.txt ]; then
-	mv -f gwys.txt gwys.txt.orig
-fi
+/bin/rm -f gwys.txt
 
-rm -f gwys.va2uv.txt
+echo "# Downloaded from dschost1.w6kd.com `date`" > gwys.txt
+awk '$1 ~ /^REF/ { printf "%s %s 20001\n", $1, $2 }' DPlus_Hosts.txt >> gwys.txt
+awk '$1 ~ /^XRF/ { printf "%s %s 30001\n", $1, $2 }' DExtra_Hosts.txt >> gwys.txt
+awk '$1 ~ /^DCS/ { printf "%s %s 30051\n", $1, $2 }' DCS_Hosts.txt >> gwys.txt
 
-wget -nv -O gwys.va3uv.txt http://www.va3uv.com/gwys.txt
-
-if [ -e gwys.va3uv.txt ]; then
-	echo "# from www.va3uv.com on `date`" > gwys.txt
-	echo "Got `awk '$1~/^REF/{print $1, $2, $3}' gwys.va3uv.txt | tee -a gwys.txt | wc -l` REF reflectors"
-	echo "Got `awk '$1~/^XRF/{print $1, $2, $3}' gwys.va3uv.txt | tee -a gwys.txt | wc -l` XRF reflectors"
-	echo "Got `awk '$1~/^DCS/{print $1, $2, $3}' gwys.va3uv.txt | tee -a gwys.txt | wc -l` DCS reflectors"
-else
-	echo "Could not get gateways list from www.va3uv.com!"
-	if [ -e gwys.txt.orig ]; then
-		mv -f gwys.txt.orig gwys.txt
-	fi
-fi
-
+/bin/rm -f D{Extra,Plus,DSC}_Hosts.txt
