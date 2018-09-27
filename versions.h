f15de760ba3588c9866cc299eb6ceaf795f27ab6
Tom Early <n7tae@tearly.net>
2018-09-27 15:24:33 -0700

    some version bumps

--- versions.h
+++ versions.h
@@ -1,8 +1,8 @@
 // version strings must be 55 characters or less!
-#define IRCDDB_VERSION "QnetGateway-7.0.1"
+#define IRCDDB_VERSION "QnetGateway-7.1.0"
 #define LINK_VERSION "QnetLink-6.0.2"
-#define DVAP_VERSION "QnetDVAP-5.1.1"
-#define RELAY_VERSION "QnetRelay-0.2.1"
+#define DVAP_VERSION "QnetDVAP-5.1.2"
+#define RELAY_VERSION "QnetRelay-0.2.2"
 #define ITAP_VERSION "QnetITAP-0.2.0"
 #define DVRPTR_VERSION "QnetDVRPTR-5.1.0"
 #define MMDVM_VERSION "QnetGateway-MMDVM-0.1.0"
