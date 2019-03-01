9c6ba07d1fe610410b5a51c01c0d4953b426e264
Tom Early <n7tae@tearly.net>
2019-02-28 17:48:04 -0700

    fixes for readFrom20000

--- QnetDVRPTR.cpp
+++ QnetDVRPTR.cpp
@@ -2127,11 +2127,12 @@ static void readFrom20000()
 				}
 
 				/* check the module and gateway */
-				if (recv_buf.hdr.rpt2[7] != DVRPTR_MOD) {
+				if (recv_buf.hdr.rpt1[7] != DVRPTR_MOD) {
+					fprintf(stderr, "rpt1=%.8s != %c, ignoring\n", recv_buf.hdr.rpt1, DVRPTR_MOD);
 					FD_CLR (fd, &readfd);
 					break;
 				}
-				memcpy(recv_buf.hdr.rpt1, DVCALL_and_G, 8);
+				memcpy(recv_buf.hdr.rpt2, DVCALL_and_G, 8);
 
 				if (memcmp(RPTR, DVCALL, CALL_SIZE) != 0) {
 					memcpy(recv_buf.hdr.rpt1, RPTR, 7);
@@ -2190,8 +2191,8 @@ static void readFrom20000()
 				}
 				recv_buf.hdr.flag[1] = recv_buf.hdr.flag[2] = 0x0;
 				memcpy(Send_Modem_Header + 8, recv_buf.hdr.flag, 3);
-				memcpy(Send_Modem_Header + 11, recv_buf.hdr.rpt2, 8);
-				memcpy(Send_Modem_Header + 19, recv_buf.hdr.rpt1, 8);
+				memcpy(Send_Modem_Header + 11, recv_buf.hdr.rpt1, 8);
+				memcpy(Send_Modem_Header + 19, recv_buf.hdr.rpt2, 8);
 				memcpy(Send_Modem_Header + 27, recv_buf.hdr.urcall, 8);
 				memcpy(Send_Modem_Header + 35, recv_buf.hdr.mycall, 8);
 				memcpy(Send_Modem_Header + 43, recv_buf.hdr.sfx, 4);
