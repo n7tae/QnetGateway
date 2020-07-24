08e5bc7e3ef49db5d78fa1021bfd32315097b56b
Tom Early <n7tae@tearly.net>
2020-07-24 06:39:08 -0700

    join

--- Utilities.h
+++ Utilities.h
@@ -5,19 +5,13 @@
 // trim from start (in place)
 static inline void ltrim(std::string &s)
 {
-	s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int ch)
-	{
-		return !std::isspace(ch);
-	}));
+	s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int ch) { return !std::isspace(ch); }));
 }
 
 // trim from end (in place)
 static inline void rtrim(std::string &s)
 {
-	s.erase(std::find_if(s.rbegin(), s.rend(), [](int ch)
-	{
-		return !std::isspace(ch);
-	}).base(), s.end());
+	s.erase(std::find_if(s.rbegin(), s.rend(), [](int ch) { return !std::isspace(ch); }).base(), s.end());
 }
 
 // trim from both ends (in place)
